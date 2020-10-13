3279061fb9491b5817170d5328a46260c53650d8
EEZHI <zeddymamman@gmail.com>
2020-10-13 22:16:14 +0100

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <app-nav></app-nav>
+  </div>
+  <router-view class = 'container'/>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  },
+};
+</script>
+<style lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable max-len-->
+<template>
+<div class = "home ">
+    <div class="jumbotron">
+  <h1 class="display-3">Welcome to Bingham University Forum</h1>
+  <p class="lead">This is a community to help </p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
